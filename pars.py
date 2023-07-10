d418c40bb88910664912c2dd58f99f4f36ad506c
kumaroid <113765449+kumaroid@users.noreply.github.com>
2023-07-10 19:26:16 +0300

    Update pars.py

--- pars.py
+++ pars.py
@@ -1,29 +1,54 @@
 from bs4 import BeautifulSoup
-from urlib.request import urlopen
+import datetime
+import time
 import requests
+from config import token
 
+def get_offset(group_id):
+    params = {'access_token': token, 'group_id': group_id, 'v': 5.131}
+    r = requests.get('https://api.vk.com/method/groups.getMembers', params=params)
+    count = r.json()['response']['count']
+    print(f'Количество подписчиков: {count}')
+    if count > 1000:
+        return count // 1000
+    else:
+        count = 1
+        return count
 
-group_id = '6121396'
-access_token = 'vk1.a.gSEyIt-pAWkxCThAX1O2JbLKrPFJny8Btyuzlj4urbFOEmF35alnDZxjxOy66yh5zadoGgvrtLxS1zZnW2rId5TcALW8GbFNYOHi1HD43Cwt60bF6QR_9ZE0iKzkr2g9_dYnGO7js3zQfx9M27r5kIQAu67IC_REZ95szDyWbKr-BGGNVsbx5WCbEGkO1_ZjiDaTA9cDy9xPlDI6CuC0Qg' 
-#мой
+def get_users(group_id, from_data):
+    """Получаем всех участников группы и фильтруем от неактивных"""
+    active_users = []
+    un_active_list = []
+    for offset in range(0, get_offset(group_id)+1):
+        params = {'access_token': token, 'v': 5.131, 'group_id': group_id, 'offset': offset*1000, 'fields': 'last_seen'}
+        users = requests.get('https://api.vk.com/method/groups.getMembers', params=params).json()['response']
+        for user in users['items']:
+            start_point_data = datetime.datetime.strptime(from_data, '%d.%m.%Y').timestamp()
+            try:
+                if user['last_seen']['time'] >= start_point_data:
+                    active_users.append({'id': user['id'], 'name': user['first_name'] + ' ' + user['last_name'], 'likes': user['likes']['count'], 'reposts': user['reposts']['count'], 'comments': user['comments']['count']})
+                else:
+                    un_active_list.append(user['id'])
+            except:
+                continue
+    active_users.sort(key=lambda x: x['likes'] + x['reposts'] + x['comments'], reverse=True)
+    return active_users[:3]
 
-#cообществ"vk1.a.Mro2GuCly-fjpf8vtq5BEJP3_TO1040E42Ek4-jOLYAFY7LdT9KeG028jVjLwCE8eakUwq8ugwM7Mez9q3bSU_FjDCru8Jnz1-BLUCXI8LEl_EkV9aOzttYdGD9miNS8_bDqIhupLGM8_Iv9LlnJfSh7naFogl9lvsIeIV8uoXXJTp7Bc0fCFDR5fc7IHas5NvjfJrOgcFCdPOzH_eusbA"
-url = f"https://api.vk.com/method/groups.getMembers?group_id={group_id}&access_token={access_token}&v=5.131"
+def parser(group_list):
+    from_data = input('Введите дату, с которой хотите отслеживать активность\nв формате: дд.мм.гггг: ')
+    print(f'Анализируем с {from_data}\n')
+    for group in group_list:
+        print(f'Группа: {group}')
+        try:
+            top_users = get_users(group, from_data=from_data)
+            print('Топ-3 самых активных пользователей:')
+            for user in top_users:
+                print(f'ID: {user["id"]}, Имя: {user["name"]}, Лайки: {user["likes"]}, Репосты: {user["reposts"]}, Комментарии: {user["comments"]}')
+            time.sleep(2)
+        except Exception as ex:
+            print(f'{group} - не предвиденная ошибка: {ex}\n')
+            continue
 
-response = requests.get(url)
-data = response.json()
-
-if "response" in data:
-    members_count = data["response"]["count"]
-    members = data["response"]["items"]
-
-soup = BeautifulSoup(str(data), "html.parser")
-
-print(f"Количество участников: {members_count}")
-print("Список участников:")
-for member in members:
-    member_id = soup.find(text=str(member))
-    print(member_id)
-
-else:
-    print("Ошибка при получении данных")ю
+if __name__ == '__main__':
+    group_list = [...] #указать айди необходимой группы(можно несколько)
+    parser(group_list)
